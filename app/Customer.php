fc141b01234e839986a82b8a9a51bec52cbd4ab4
jinmy <solisjinmy@gmail.com>
2017-03-14 11:02:46 -0430

    configuracion modelos

--- app/Customer.php
+++ app/Customer.php
@@ -3,8 +3,22 @@
 namespace App;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class Customer extends Model
 {
-    //
+    use SoftDeletes;
+    
+    protected $fillable = [
+       'dni', 'name', 'last_name', 'address', 'phone', 'mobile', 'email',
+    ];
+    
+    protected $appends=['full_name'];
+    
+    public function getFullNameAttribute()
+    {
+        return trim($this->name). ''.trim($this->last_name);
+    }
 }
+
+ 
